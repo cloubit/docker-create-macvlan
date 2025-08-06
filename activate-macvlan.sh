c3cfac9360f15c0c064eb8588014b51a8d859389
Cloubit <development@cloubit.net>
2025-08-06 13:23:16 +0200

    Update activate-macvlan.sh
    Optimization

--- activate-macvlan.sh
+++ activate-macvlan.sh
@@ -1,52 +1,51 @@
 #!/bin/sh
 
-# Define Docker variables:
-DOCKERPATH=/usr/bin/docker
-
 # Define network variables:
-DRIVER=macvlan
 SUBNET=10.10.10.0/24
 IPRANGE=10.10.10.128/28
 GATEWAY=10.10.10.1
 ADAPTER=my-lan-adapter
 NETWORKNAME=my-macvlan-name
-macvlan_mode=bridge
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME Script started successfully" >> $NETWORKNAME.log
+# Define Docker variables:
+DOCKERPATH=/usr/bin/docker
 
-while true;
-    do
-    # check docker status.
-    while ! $DOCKERPATH info > /dev/null 2>&1;
-        do
-            echo "$(date '+%Y-%m-%d %H:%M:%S') Docker is inactiv." >> $NETWORKNAME.log
-            sleep 5
-    done
-        echo "$(date '+%Y-%m-%d %H:%M:%S') Docker is activ." > /dev/null
-    # Check if Docker MacVlan available.
+# check docker status.
+while ! $DOCKERPATH info > /dev/null 2>&1;
+do
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Docker is inactiv." >> $NETWORKNAME.service.log
+    sleep 5
+done
+echo "$(date '+%Y-%m-%d %H:%M:%S') Docker is activ." >> $NETWORKNAME.service.log
+
+# Check if Docker MacVlan available.
+if docker network inspect $NETWORKNAME | grep -i "$NETWORKNAME" 
+    then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME is activ." >> $NETWORKNAME.service.log
+else
+    # Create docker MacVlan.
+    docker network create --driver macvlan --subnet $SUBNET --gateway $GATEWAY --ip-range=$IPRANGE --opt parent=$ADAPTER $NETWORKNAME
+    sleep 3
+    # Check if Docker macvlan was created.
     if docker network inspect $NETWORKNAME | grep -i "$NETWORKNAME" 
         then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME is activ." > /dev/null
+        echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME was created." >> $NETWORKNAME.service.log
     else
-        # Create docker MacVlan.
-        docker network create --driver $DRIVER --subnet $SUBNET --gateway $GATEWAY --ip-range=$IPRANGE --opt parent=$ADAPTER $NETWORKNAME
-        sleep 3
-        # Check if Docker macvlan was created.
-        if docker network inspect $NETWORKNAME | grep -i "$NETWORKNAME" 
-            then
-            echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME was created." >> $NETWORKNAME.log
-        else
-            echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME was created." >> $NETWORKNAME.log
-        fi
+        echo "$(date '+%Y-%m-%d %H:%M:%S') $NETWORKNAME was not created." >> $NETWORKNAME.service.log
     fi
-    # Check is promiscuous mode active.
+fi
+
+# Check is promiscuous mode active.
+if ip a | grep -i "$ADAPTER.*PROMISC"
+    then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Promiscuous mode on adapter $ADAPTER is active." >> $NETWORKNAME.service.log
+else
+    ip link set $ADAPTER promisc on
     if ip a | grep -i "$ADAPTER.*PROMISC"
         then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') Promiscuous mode on adapter $ADAPTER is active." > /dev/null
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Promiscuous mode on adapter $ADAPTER is active." >> $NETWORKNAME.service.log
     else
-        ip link set $ADAPTER promisc on
-        echo "$(date '+%Y-%m-%d %H:%M:%S') Promiscuous mode on adapter $ADAPTER was configured." >> $NETWORKNAME.log
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Promiscuous mode on adapter $ADAPTER is not active" >> $NETWORKNAME.service.log
     fi
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Script run successfully. Check output before." >> $NETWORKNAME.log
-    sleep 120 
-done
+fi
+echo "$(date '+%Y-%m-%d %H:%M:%S') Script has run. Check output before." >> $NETWORKNAME.service.log
